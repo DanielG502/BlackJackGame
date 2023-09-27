70beca06034886dc0e189b07b8e6139e9e30dc78
Jorge Gonzalez <jdanielgonzaleze@gmail.com>
2023-09-26 20:25:34 -0600

    Finishing the game BlackJAck in the lenguaje JavaScript

---
+++ README copy.md
@@ -0,0 +1,5 @@
+# Modificando el Juego BlackJack
+# 1 Utilizando las funciones anonimas 
+para protejer el codigo y no se pueda utilizar en la consola del navegador
+# 2 Optimizacion Primera Parte
+Refactorizar el codigo 
---
+++ public/assets/JavaScript/juego.js
@@ -0,0 +1,147 @@
+(() => {
+    // esto es una funcion anonima!
+    'use strict';
+
+    let deck = [];
+    const tipos = ['C', 'D', 'H', 'S'],
+        especiales = ['A', 'J', 'Q', 'K'];
+
+    let puntosJugadores = [];
+
+    // Referencias del HTML
+    const btnPedir = document.querySelector('#btnPedir'),
+        btnDetener = document.querySelector('#btnDetener'),
+        btnNuevo = document.querySelector('#btnNuevo');
+
+    const divCartasJugadores = document.querySelectorAll('.divCartas'),
+        puntosHTML = document.querySelectorAll('small');
+
+    const inicializarJuego = (numJugadores = 2) => {
+        deck = crearDeck();
+        puntosJugadores = [];
+
+        for (let j = 0; j < numJugadores; j++) {
+            puntosJugadores.push(0);
+        }
+
+        puntosHTML.forEach((elem) => (elem.innerText = 0));
+        divCartasJugadores.forEach((elem) => (elem.innerHTML = ''));
+
+        btnPedir.disabled = false;
+        btnDetener.disabled = false;
+    };
+
+    // Crea nuestra baraja
+    const crearDeck = () => {
+        deck = [];
+        for (let i = 2; i <= 10; i++) {
+            for (let tipo of tipos) {
+                deck.push(i + tipo);
+            }
+        }
+
+        for (let tipo of tipos) {
+            for (let esp of especiales) {
+                deck.push(esp + tipo);
+            }
+        }
+        return _.shuffle(deck);
+    };
+
+    // Esta funcion me permite tomar una carta
+
+    const pedirCarta = () => {
+        if (deck.length === 0) {
+            throw 'No hay cartas en el Deck';
+        }
+        return deck.pop(); //remueve el ultimo elemento y lo regresa
+    };
+
+    const valorCarta = (carta) => {
+        const valor = carta.substring(0, carta.length - 1);
+        return isNaN(valor) ? (valor === 'A' ? 11 : 10) : valor * 1;
+    };
+
+    // 0 = primer jugador y el ultimo sera la computadora
+    const acumularPuntos = (carta, turno) => {
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta(carta);
+        puntosHTML[turno].innerText = puntosJugadores[turno];
+        return puntosJugadores[turno];
+    };
+
+    const crearCarta = (carta, turno) => {
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `cartas/${carta}.png`; //obtenemos las cartas
+        imgCarta.classList.add('carta'); //ingresamos la clase de la carta
+        divCartasJugadores[turno].append(imgCarta);
+    };
+    // 18031263 Quetzalez
+    // 18031269 USD
+
+    const determinarGanador = () => {
+        const [puntosMinimos, puntosComputadora] = puntosJugadores;
+        setTimeout(() => {
+            if (puntosComputadora === puntosMinimos) {
+                alert('Nadie Gana');
+            } else if (puntosMinimos > 21) {
+                alert('Computadora Gana');
+            } else if (puntosComputadora > 21) {
+                alert('Jugador Gana');
+            } else {
+                alert('Computadora Gana');
+            }
+        }, 100);
+    };
+
+    const turnoComputadora = (puntosMinimos) => {
+        let puntosComputadora = 0;
+
+        do {
+            const carta = pedirCarta();
+            puntosComputadora = acumularPuntos(
+                carta,
+                puntosJugadores.length - 1
+            );
+            crearCarta(carta, puntosJugadores.length - 1);
+        } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+
+        determinarGanador();
+    };
+
+    // Eventos
+    btnPedir.addEventListener('click', () => {
+        const carta = pedirCarta();
+        const puntosJugador = acumularPuntos(carta, 0);
+        crearCarta(carta, 0);
+
+        if (puntosJugador > 21) {
+            console.warn('Lo siento mucho, Perdiste!');
+            btnPedir.disabled = true; //inhabilitamos el boton
+            btnDetener.disabled = true;
+            turnoComputadora(puntosJugador);
+        } else if (puntosJugador === 21) {
+            console.warn('Ganaste!!');
+            btnPedir.disabled = true;
+            btnDetener.disabled = true;
+            turnoComputadora(puntosJugador);
+        }
+    });
+
+    // Funcion Boton Detener
+
+    btnDetener.addEventListener('click', () => {
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugadores[0]);
+    });
+
+    // Funcion Boton Nuevo Juevo
+
+    btnNuevo.addEventListener('click', () => {
+        inicializarJuego();
+    });
+})();
+
+// otro tipo de funcion anonima
+// (function() {
+// })()
